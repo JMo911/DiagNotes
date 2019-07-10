73a82a57935d2c332ba757bf44b5763037415607
Jacob Moss <jacobmoss09@gmail.com>
2019-07-10 18:22:28 -0500

    pushing latest commits

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,41 @@
 // TESTING CONNECTION$(".jumbotron").text("this test text");
+// var queryurl=""
+
+// $.ajax({
+//     url: queryurl,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
+// });
+
+
+// TESTING SPEECH RECOGNITION AREA
+  var recognizing;
+  var recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  reset();
+  recognition.onend = reset;
+
+  recognition.onresult = function (event) {
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        textarea.value += event.results[i][0].transcript;
+      }
+    }
+  }
+
+  function reset() {
+    recognizing = false;
+    button.innerHTML = "Click to Speak";
+  }
+
+  function toggleStartStop() {
+    if (recognizing) {
+      recognition.stop();
+      reset();
+    } else {
+      recognition.start();
+      recognizing = true;
+      button.innerHTML = "Click to Stop";
+    }
+  }
